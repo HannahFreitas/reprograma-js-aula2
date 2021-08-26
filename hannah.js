5a1456a0e6f853720198ed58ed538b142a887e10
HannahFreitas <hannahcassia@gmail.com>
2021-08-26 00:08:35 -0300

    Aula 2 reprograma

---
+++ hannah.js
@@ -0,0 +1,5 @@
+var listaDeCompras = ["cebola", " tomate", " alface", " papel toalha", " manga", " abacaxi", " feijão", " arroz"];
+alert(listaDeCompras);
+alert("Vamos somar 87 + 96 = " + (87 + 96));
+alert("Vamos multiplicar 5 x 8 = " + (5 * 8));
+alert("Seja bem vinda ao meu site!");
